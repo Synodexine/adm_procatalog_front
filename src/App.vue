255d192bfa18869fb60dfa8c8cc175dd6cbc7fb6
Ahoskie <worka.zip@gmail.com>
2021-04-30 16:23:16 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+      <NavigationBar></NavigationBar>
+  </div>
+</template>
+
+<script>
+import NavigationBar from './components/navBar/NavigationBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavigationBar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/navBar/NavigationBar.vue
@@ -0,0 +1,39 @@
+<template>
+    <b-navbar class="nav-bar" variant="dark">
+        <b-navbar-brand href="#">
+            <div class="nav-text">Product catalog</div>
+        </b-navbar-brand>
+
+        <b-navbar-nav>
+            <b-nav-item href="#">
+                <div class="nav-text">Search</div>
+            </b-nav-item>
+            <b-nav-item href="#">
+                <div class="nav-text">Documentation</div>
+            </b-nav-item>
+        </b-navbar-nav>
+
+        <b-navbar-nav class="ml-auto">
+            <b-nav-item-dropdown text="User" class="nav-text" right>
+                <b-dropdown-item href="#">Account</b-dropdown-item>
+                <b-dropdown-item href="#">Settings</b-dropdown-item>
+            </b-nav-item-dropdown>
+        </b-navbar-nav>
+    </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'NavigationBar'
+}
+</script>
+
+<style scoped>
+.nav-bar {
+    color: white;
+    /* background-color: #8fb2e3; */
+}
+.nav-text {
+    color: whitesmoke;
+}
+</style>
